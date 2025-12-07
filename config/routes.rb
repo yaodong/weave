32b8d8969b268f1a0d7011fb0b546b80854591b0
Yaodong Zhao <git@yaodong.dev>
2025-12-07 14:48:31 -0800

    Add root controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "landing#index"
+
+  get "home" => "home#index", as: :home
 end
